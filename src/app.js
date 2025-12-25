1ccd29f6207a7530eba438ecb1f05248d8fc48d1
vishwaksuda-16 <vishwaksuda@gmail.com>
2025-12-25 20:26:11 +0530

    Setup Neon Postgres with Drizzle

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
-
+import logger from './config/logger.js';
 const app = express();
 
 app.get('/', (req, res) => {
+  logger.info("Hello from Acquisitions!");
+  
   res.status(200).send('Hello from Aquisitions!');
 });
 
