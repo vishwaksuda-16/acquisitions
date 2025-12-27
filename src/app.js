ae962059edefb3f6e05cecf1ba6a8228b9bf05ab
vishwaksuda-16 <vishwaksuda@gmail.com>
2025-12-27 12:45:42 +0530

    Implement Dockerization

--- src/app.js
+++ src/app.js
@@ -17,7 +17,6 @@ app.use(cookieParser());
 
 app.use(morgan('combined', {stream: {write: (message) => logger.info(message.trim() )} }))
 
-app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info("Hello from Acquisitions!");
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from './app.js';
 
-const PORT = process.env.PORT || 2005;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
